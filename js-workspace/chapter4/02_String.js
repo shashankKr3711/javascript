ba4f4afab05a21f4f38a3301f93716bdb0858a7e
shashankkr3711 <shashank.kumar3711@gmail.com>
2022-08-04 00:27:26 +0530

    updated for 02_String.js completed Strings

---
+++ js-workspace/chapter4/02_String.js
@@ -0,0 +1,13 @@
+let myname = 'shashank';
+console.log("lower case name : ",myname.toLowerCase());
+console.log("Upper case name : ",myname.toUpperCase());
+console.log("Example of slice : ",myname.slice(2, 7));// prints 2 to 6.
+console.log("Example of slice : ",myname.slice(2));// prints 2 to end.
+console.log("Example of slice : ",myname.replace('has', 'o'));// prints 2 to end.
+
+let friend = 'Nitesh Nishan';
+console.log('Example of concat : '+myname.concat(friend).concat('Hello'));
+const nameWithSpace = '            Meena                 ';
+console.log('remove space with trim : '+nameWithSpace.trim());
+
+
