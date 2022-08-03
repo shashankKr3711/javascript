e20bcc25736d5e8ada690fbb90ea54c1560d94d0
shashankkr3711 <shashank.kumar3711@gmail.com>
2022-08-04 00:11:11 +0530

    first commit for 02_String and methods added in 01_string.js

---
+++ js-workspace/chapter4/01_String.js
@@ -0,0 +1,19 @@
+let myName = "Shashank";
+let friendname = 'Nitesh';
+console.log(myName)
+// let enemyname = "Jhon' // dont do this
+console.log('My friend name : '+myName)
+console.log('length of the given name is : ',myName.length)
+
+console.log('myName 1st index is : '+myName[1]);
+
+let boy1 = 'Promod';
+let boy2 = 'Shashank';
+
+//String interpolation
+let sentence =`${boy2} is friend of ${boy1}`;
+console.log(sentence);
+
+// Escape sequence
+let fruitSentence = 'apple is shashank\'s fruit';
+console.log(fruitSentence);
