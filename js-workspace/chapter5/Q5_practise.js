0eb0c91c24a45355d9fad3fb7bcab2ae3609ab99
shashankkr3711 <shashank.kumar3711@gmail.com>
2022-08-14 23:45:41 +0530

    fix Q5 added return in the function

--- js-workspace/chapter5/Q5_practise.js
+++ js-workspace/chapter5/Q5_practise.js
@@ -3,8 +3,8 @@
 let n = 5;
 let arr = [1,2,3,4,5];
 const reduceFact = (myArr)=>{
-    myArr.reduce((n1, n2)=>{
+    return myArr.reduce((n1, n2)=>{
         return n1*n2;
     });
 }
-console.log(reduceFact())
+console.log(reduceFact(arr));
